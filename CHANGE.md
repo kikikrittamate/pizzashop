025874ad3bcc590f09096c7c68f5ccaf43b57de6
kikikrittamate <krittamate.ch@ku.th>
2021-10-11 12:04:19 +0700

    finished

--- CHANGE.md
+++ CHANGE.md
@@ -21,23 +21,28 @@ There are many ways. One solution is to add the per-topping price to PizzaSize.
 The value of an Enum member can be *anything*, not just a number. We can set it to a dictionary of attributes for the base price and topping price:
 ```python
 class PizzaSize(Enum):
-    small =  {'base_price': 120, 'topping': 20}
+    small = {'base_price': 120, 'topping': 20}
     medium = {'base_price': 200, 'topping': 25}
-    large =  {'base_price': 300, 'topping': 30}
+    large = {'base_price': 300, 'topping': 30}
 
     @property
     def price(self):
         return self.value['base_price']
-    
+
     @property
     def topping_price(self):
         return self.value['topping']
+
+    def __str__(self):
+        return self.name
 ```
 
 and in Pizza.get_price:
 ```python
-   def get_price(self):
-       return self.size.price + self.size.topping_price*len(self.toppings)
+def get_price(self):
+    """Price of pizza depends on size and number of toppings."""
+    price = self.size.price + 20 * len(self.toppings)
+    return price
 ```
 
 ### Benefit of Encapsulation
--- pizza.py
+++ pizza.py
@@ -1,24 +1,51 @@
+from enum import Enum
+
+
+class PizzaSize(Enum):
+    # Enum members written as: name = value
+    small = 120
+    medium = 200
+    large = 280
+
+    @property
+    def price(self):
+        return self.value
+
+    @property
+    def topping_price(self):
+        return self.value['topping']
+
+    def __str__(self):
+        return self.name
+
+
 class Pizza:
     """A pizza with a size and optional toppings."""
 
     def __init__(self, size):
+        if not isinstance(size, PizzaSize):
+            raise TypeError("Size must be a PizzaSize")
         self.size = size
         self.toppings = []
 
-    def getPrice(self):
+    def get_price(self):
         """Price of pizza depends on size and number of toppings."""
-        if self.size == 'small':
-            price = 120 + 20*len(self.toppings)
-        elif self.size == 'medium':
-            price = 200 + 20*len(self.toppings)
-        elif self.size == 'large':
-            price = 280 + 20*len(self.toppings)
-        else:
-            raise ValueError("Unknown pizza size "+self.size)
+        price = self.size.price + 20 * len(self.toppings)
         return price
     
-    def addTopping(self, topping):
+    def add_topping(self, topping):
         """Add a topping to the pizza"""
         if topping not in self.toppings:
             self.toppings.append(topping)
 
+    def __str__(self):
+        description = self.size.name
+        if self.toppings:
+            description += " pizza with " + ", ".join(self.toppings)
+        else:
+            description += " plain cheese pizza"
+        return description
+
+    @property
+    def price(self):
+        return self.value
